236838c9aee8aa03ffb0041c2d8148bc426a9af6
DragonYellow <786676631@qq.com>
2021-07-20 10:50:12 +0800

    test更新
    Signed-off-by: DragonYellow <786676631@qq.com>

--- JS设计模式/test/developTest.js
+++ JS设计模式/test/developTest.js
@@ -1,52 +1,13 @@
-/**
- * 代理模式
- */
+let map = new Map();
 
-function Instance() {
- 
-}
-
-Instance.getInstance = function () {
-  if (!this.instance) {
-    this.instance = new Instance()
+let proxy = new Proxy(map, {
+  get(target, prop, receiver) {
+    console.log(arguments)
+    let value = Reflect.get(...arguments);
+    console.log(value)
+    return typeof value == 'function' ? value.bind(target) : value;
   }
+});
 
-  return this.instance
-}
-
-function mul(args) {
-  let a = 1;
-  console.log(args)
-  for (let v of args) {
-    a *= v;
-  }
-
-  return a
-}
-
-function proxyTest(fn) {
-  let obj = Instance.getInstance()
-
-  return new Proxy(fn, {
-    apply(target, thisArg, args) {
-      const prop = args.join(',')
-      console.log(obj)
-
-      if (obj[prop]) {
-        console.log(222)
-        return obj[prop]
-      }
-
-      console.log(333)
-      obj[prop] = fn(args)
-      return obj[prop]
-    }
-  })
-}
-
-let p = proxyTest(mul)
-
-let all = p(1, 2, 3, 4)
-let all1 = p(1, 2, 3, 4)
-
-console.log(all)
+proxy.set('test', 1);
+console.log(proxy.get('test')); // 1（工作了！）
--- JS设计模式/test/proxyTest.js
+++ JS设计模式/test/proxyTest.js
@@ -4,6 +4,10 @@
  * func.apply(context, args)
  * 为其他对象提供一种代理以控制对这个对象的访问
  * 
+ * Proxy 的局限性:许多内建对象，例如 Map，Set，Date，Promise 等，都使用了所谓的“内部插槽”。
+ * 它们类似于属性，但仅限于内部使用，仅用于规范目的
+ * 内建方法可以直接访问它们，而不通过 [[Get]]/[[Set]] 内部方法。所以 Proxy 无法拦截它们。
+ * 
  * Reflect:
  * 对于每个可被 Proxy 捕获的内部方法，在 Reflect 中都有一个对应的方法，
  * 其名称和参数与 Proxy 捕捉器相同。
@@ -11,6 +15,21 @@
  * 给target设置属性的值
  * Reflect.set(target, prop, value);
  */
+
+
+// proxy.set(...) 内部 this 的值并不是 proxy，而是原始的 map。因此，
+// 当set 捕捉器的内部实现尝试访问 this.[[MapData]] 内部插槽时，它会成功。
+ let map = new Map();
+
+ let proxy = new Proxy(map, {
+   get(target, prop, receiver) {
+     let value = Reflect.get(...arguments);
+     return typeof value == 'function' ? value.bind(target) : value;
+   }
+ });
+ 
+ proxy.set('test', 1);
+ alert(proxy.get('test')); // 1（工作了！）
 
 
 // 缓存代理实现乘积计算
@@ -65,13 +84,13 @@ const validators = {
 
 const getValidateProxy = (target, validators) => {
   return new Proxy(target, {
-    _validators: validators,
+    __validators: validators,
     set(target, prop, value) {
       if (value === '') {
         console.error(`"${prop}" is not allowed to be empty`);
         return target[prop] = false;
       }
-      const validResult = this._validators[prop](value);
+      const validResult = this.__validators[prop](value);
       if (validResult.valid) {
         // 对于每个可被 Proxy 捕获的内部方法，在 Reflect 中都有一个对应的方法，
         // 其名称和参数与 Proxy 捕捉器相同。
@@ -89,4 +108,4 @@ const getValidateProxy = (target, validators) => {
 }
 
 const userFormProxy = getValidateProxy(userForm, validators);
-userFormProxy.account = '在'; // "account" is only allowed to be ChineseuserFormProxy.password = 'he'; 
+userFormProxy.account = '123'; // "account" is only allowed to be ChineseuserFormProxy.password = 'he'; 
